4ed5d211aac1d9bb324c2e7e3ec9301ce5b69f6e
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-16 16:03:21 +0530

    using a function to increase or decrease the like and dislike counts

--- js/main.js
+++ js/main.js
@@ -1,42 +1,44 @@
 $(document).ready(function(){
+    function decrease_like(){
+        var like_count = $("#like_count").html();
+        like_count--;
+        $("#like_count").html(like_count);
+    }
+    function decrease_dislike(){
+        var dislike_count = $("#dislike_count").html();
+        dislike_count--;
+        $("#dislike_count").html(dislike_count);
+    }
     $("#like").click(function(){
     if($("#like").hasClass("text-success")){
-    		$("#like").removeClass("text-success");	
-    		var like_count = $("#like_count").html();
-	        like_count--;
-        	$("#like_count").html(like_count);
-    	}
-    	else{
-	        $("#like").addClass("text-success");
-	        if($("#dislike").hasClass("text-danger")){
-		        $("#dislike").removeClass("text-danger");
-	        	var dislike_count = $("#dislike_count").html();
-	        	dislike_count--;
-        		$("#dislike_count").html(dislike_count);
-	        }
-	        var like_count = $("#like_count").html();
-	        like_count++;
-	        $("#like_count").html(like_count);
+		$("#like").removeClass("text-success");	
+		decrease_like();
+	}
+	else{
+        $("#like").addClass("text-success");
+        if($("#dislike").hasClass("text-danger")){
+	        $("#dislike").removeClass("text-danger");
+            decrease_dislike();
         }
+        var like_count = $("#like_count").html();
+        like_count++;
+        $("#like_count").html(like_count);
+    }
     });
     $("#dislike").click(function(){
-    	if($("#dislike").hasClass("text-danger")){
-    		$("#dislike").removeClass("text-danger");
-    		var dislike_count = $("#dislike_count").html();
-	        dislike_count--;
-        	$("#dislike_count").html(dislike_count);	
-    	}
-    	else{
-	        $("#dislike").addClass("text-danger");
-	        if($("#like").hasClass("text-success")){
-				var like_count = $("#like_count").html();
-		        like_count--;
-	        	$("#like_count").html(like_count);
-			}
-	        $("#like").removeClass("text-success");
-	        var dislike_count = $("#dislike_count").html();
-        	dislike_count++;
-       		$("#dislike_count").html(dislike_count);
-        }
+	if($("#dislike").hasClass("text-danger")){
+		$("#dislike").removeClass("text-danger");
+        decrease_dislike();	
+	}
+	else{
+        $("#dislike").addClass("text-danger");
+        if($("#like").hasClass("text-success")){
+            decrease_like();
+		}
+        $("#like").removeClass("text-success");
+        var dislike_count = $("#dislike_count").html();
+    	dislike_count++;
+   		$("#dislike_count").html(dislike_count);
+    }
     });
 });
