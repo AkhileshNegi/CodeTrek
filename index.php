e9d93b8bcdf42c9fade046d48f4fe1a8ed60ecab
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-22 13:14:41 +0530

    added new file for signing out

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ if ($conn->connect_error){
 							<a class="dropdown-item" href="Profile.php">My Profile</a>
 							<a class="dropdown-item" href="#">Settings</a>
 							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Sign out</a>
+							<a class="dropdown-item" href="sign_out.php">Sign out</a>
 						</div>
 					</li>
 				</ul>
